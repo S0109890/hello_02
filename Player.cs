d6c611e74e3c2a510ca35a75243d232a9b66f15a
s0109890 <leeseungmin12@gmail.com>
2020-09-21 01:00:34 +0900

    no message

--- Player.cs
+++ Player.cs
@@ -7,12 +7,7 @@ public class Player : Actor
     // Start is called before the first frame update
     void Start()
     {
-     Console.WriteLine("hello-world")
-
-int a = 5;
-int b = 6;
-
-Console.WriteLine(a+b) 
+     Console.WriteLine("hello-world")   
     }
 
         
