f24ef3348a472d2155dd71421611e8be145c482b
s0109890 <leeseungmin12@gmail.com>
2020-09-21 01:13:05 +0900

    브랜치하여 수정함

--- Player.cs
+++ Player.cs
@@ -7,8 +7,18 @@ public class Player : Actor
     // Start is called before the first frame update
     void Start()
     {
-     Console.WriteLine("hello-world")   
+        Console.WriteLine("hello-world")
     }
+    int a = 6;
+    int b = 7;
+
+    int c = a + b;
+        Console.WriteLine(c);
+    string[] name = { "일", "이", "삼", "사" }
+    for(int=0;i<4;i++)
+        {
+        Console.WriteLine(name[i]);
+        }
 
         
     }
