d1ee343932d40022a7e0a8084db5b4519cfcfa7b
s0109890 <leeseungmin12@gmail.com>
2020-09-21 01:03:05 +0900

    Revert "no message"
    This reverts commit d6c611e74e3c2a510ca35a75243d232a9b66f15a.

--- Player.cs
+++ Player.cs
@@ -7,7 +7,12 @@ public class Player : Actor
     // Start is called before the first frame update
     void Start()
     {
-     Console.WriteLine("hello-world")   
+     Console.WriteLine("hello-world")
+
+int a = 5;
+int b = 6;
+
+Console.WriteLine(a+b) 
     }
 
         
